cf1346445be220ed3dfac163a1182d51607001f1
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-09-01 09:06:48 +0100

    Create Sherlock_and_GCD.java

---
+++ Algorithms-Warmup/Sherlock_and_GCD.java
@@ -0,0 +1,39 @@
+package Algorithms_Warmup;
+
+import java.util.Scanner;
+
+public class Sherlock_and_GCD {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+
+		for (int i = 0; i < t; i++) {
+			int N = scn.nextInt();
+			int[] a = new int[N];
+			int gc = 0;
+
+			for (int j = 0; j < N; j++) {
+				a[j] = scn.nextInt();
+				gc = gcd(gc, a[j]);
+			}
+
+			if (gc == 1) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+		}
+
+		scn.close();
+	}
+
+	private static int gcd(int m, int n) {
+		while (n > 0) {
+			int tmp = n;
+			n = m % n;
+			m = tmp;
+		}
+
+		return m;
+	}
+}
