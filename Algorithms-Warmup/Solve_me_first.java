2f5dc7f729feeb1a7043a5bbe3da3f1cced068e7
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-09-01 09:05:56 +0100

    Create Solve_me_first.java

---
+++ Algorithms-Warmup/Solve_me_first.java
@@ -0,0 +1,13 @@
+package Algorithms_Warmup;
+
+import java.util.Scanner;
+
+public class Solve_me_first {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int a = scn.nextInt();
+		int b = scn.nextInt();
+		scn.close();
+		System.out.println((a + b));
+	}
+}
