fca5331f1f35ec58a9d6182387441137af574521
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-09-01 09:05:17 +0100

    Create Utopian_Tree.java

---
+++ Algorithms-Warmup/Utopian_Tree.java
@@ -0,0 +1,35 @@
+package Algorithms_Warmup;
+
+import java.util.Scanner;
+
+public class Utopian_Tree {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int noOfTestCases = scn.nextInt();
+
+		for (int i = 0; i < noOfTestCases; i++) {
+			int noOfCycles = scn.nextInt();
+			System.out.println(UtopianHeight(noOfCycles));
+		}
+
+		scn.close();
+	}
+
+	private static int UtopianHeight(int noOfCycles) {
+		int height = 1;
+
+		if (noOfCycles <= 0) {
+			return 1;
+		}
+
+		for (int i = 1; i <= noOfCycles; i++) {
+			if (i % 2 != 0) {
+				height *= 2;
+			} else {
+				height++;
+			}
+		}
+		
+		return height;
+	}
+}
