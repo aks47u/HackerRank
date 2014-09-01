42371d0e8db2ed3169aa7113417f4d5ba037ce1f
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-09-01 09:07:21 +0100

    Create Maximizing_XOR.java

---
+++ Algorithms-Warmup/Maximizing_XOR.java
@@ -0,0 +1,25 @@
+package Algorithms_Warmup;
+
+import java.util.Scanner;
+
+public class Maximizing_XOR {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		short l = scn.nextShort();
+		short r = scn.nextShort();
+		short max = 0;
+
+		for (int a = l; a <= r; a++) {
+			for (int b = a; b <= r; b++) {
+				int curr = a ^ b;
+
+				if (curr > max) {
+					max = (short) curr;
+				}
+			}
+		}
+
+		System.out.println(max);
+		scn.close();
+	}
+}
