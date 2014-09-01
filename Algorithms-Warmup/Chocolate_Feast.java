3bff6c48f4c056a5432fe0e5f17dedd3877b3be9
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-09-01 09:09:32 +0100

    Create Chocolate_Feast.java

---
+++ Algorithms-Warmup/Chocolate_Feast.java
@@ -0,0 +1,40 @@
+package Algorithms_Warmup;
+
+import java.util.Scanner;
+
+public class Chocolate_Feast {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int numTestCases = scn.nextInt();
+
+		while (numTestCases > 0) {
+			int n = scn.nextInt();
+			int c = scn.nextInt();
+			int m = scn.nextInt();
+			String numChocolates = NumberChocolates(n, c, m);
+			System.out.println(numChocolates);
+			numTestCases--;
+		}
+
+		scn.close();
+	}
+
+	private static String NumberChocolates(int n, int c, int m) {
+		int totalChocolates = 0;
+		int baseNumChocolates = n / c;
+		totalChocolates += baseNumChocolates;
+		int additionalChocolates = baseNumChocolates / m;
+		totalChocolates += additionalChocolates;
+		int remainingWrappers = baseNumChocolates % m;
+		int totalWrappers = remainingWrappers + additionalChocolates;
+
+		while (totalWrappers >= m) {
+			baseNumChocolates = totalWrappers / m;
+			remainingWrappers = totalWrappers % m;
+			totalChocolates += baseNumChocolates;
+			totalWrappers = remainingWrappers + baseNumChocolates;
+		}
+
+		return "" + totalChocolates;
+	}
+}
