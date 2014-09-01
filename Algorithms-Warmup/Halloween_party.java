71af98b7bdf237530bcda366c7d4ba3fb0a08f06
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-09-01 09:08:09 +0100

    Create Halloween_party.java

---
+++ Algorithms-Warmup/Halloween_party.java
@@ -0,0 +1,24 @@
+package Algorithms_Warmup;
+
+import java.util.Scanner;
+
+public class Halloween_party {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		
+		for (int i = 0; i < t; i++) {
+			int k = scn.nextInt();
+			System.out.println(noOfChocolates(k));
+		}
+		
+		scn.close();
+	}
+	
+	private static long noOfChocolates(int k) {
+		long noOfChocolates = 0;
+		noOfChocolates = (long) (k / 2) * (k - (long) (k / 2));
+		
+		return noOfChocolates;
+	}
+}
