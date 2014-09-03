5f96258c2090873fe37bf4dc764e97d18d1a663a
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-09-03 08:26:50 +0100

    Create Intro_to_Tutorial_Challenges.java

---
+++ Algorithms/Arrays_and_Sorting/Intro_to_Tutorial_Challenges.java
@@ -0,0 +1,20 @@
+package Algorithms_Arrays_and_Sorting;
+
+import java.util.Scanner;
+
+public class Intro_to_Tutorial_Challenges {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int v = scn.nextInt();
+		int n = scn.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			if (scn.nextInt() == v) {
+				System.out.println(i);
+				break;
+			}
+		}
+
+		scn.close();
+	}
+}
