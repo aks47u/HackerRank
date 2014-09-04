b3c9782fdafaf874acc14e5269560ffd2dd413c5
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-09-04 08:14:11 +0100

    Create Mark_and_Toys.java

---
+++ Algorithms/Arrays-and-Sorting/Mark_and_Toys.java
@@ -0,0 +1,33 @@
+package Algorithms_Arrays_and_Sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Mark_and_Toys {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int k = scn.nextInt();
+		int arr[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = scn.nextInt();
+		}
+
+		scn.close();
+		Arrays.sort(arr);
+		int count = 0, sum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+
+			if (sum > k) {
+				break;
+			} else {
+				count++;
+			}
+		}
+
+		System.out.println(count);
+	}
+}
