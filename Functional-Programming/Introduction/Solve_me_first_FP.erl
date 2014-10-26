0f9c27cea34284b990188e70f3b1d3261c0d26c5
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2014-10-26 12:44:46 +0000

    Create Solve_me_first_FP.erl

---
+++ Functional-Programming/Introduction/Solve_me_first_FP.erl
@@ -0,0 +1,8 @@
+-module(solution).
+-export([main/0]).
+solveMeFirst(A, B) ->
+    A + B.
+main() -> 
+    {ok, [A, B]} = io:fread("", "~d~d"),
+    Res = solveMeFirst(A,B),
+    io:format("~p~n",[Res]).
