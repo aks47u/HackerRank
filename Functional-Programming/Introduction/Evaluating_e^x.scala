ab90af81cdb5d14140fb4a9c9520ee1746467e3a
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2014-10-27 13:02:39 +0000

    Create Evaluating_e^x.scala

---
+++ Functional-Programming/Introduction/Evaluating_e^x.scala
@@ -0,0 +1,27 @@
+def f(x: Float): Float = {
+	se(x, 9);
+}
+
+def fact(x: Int): Int = {
+	if (x == 0){
+		return 1;
+	}
+	
+	return x * fact(x-1);
+}
+
+def pow(base: Float, power: Int): Float = {
+	if (power == 0){
+		return 1;
+	}
+	
+	return base * pow(base, power-1);
+}
+
+def se(x: Float, steps: Int): Float = {
+	if (steps == 0){
+		return 1;
+	}
+	
+	return pow(x, steps)/fact(steps).toFloat + se(x, steps-1);
+}
