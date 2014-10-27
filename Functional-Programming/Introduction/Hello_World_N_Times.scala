4675519a89e593e6025d4bd4f213b1cddcb68469
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2014-10-27 12:57:53 +0000

    Update Hello_World_N_Times.scala

--- Functional-Programming/Introduction/Hello_World_N_Times.scala
+++ Functional-Programming/Introduction/Hello_World_N_Times.scala
@@ -1,10 +1,5 @@
-object Solution extends App {
-	def f(n : Int) = {
-	    for(i <- 0 until n){
-	        println("Hello World");
-	    }
-	}
-	
-	var n = readInt
-  	f(n)
+def f(n : Int) = {
+    for(i <- 0 until n) {
+        println("Hello World");
+    }
 }
