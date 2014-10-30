e36ca9acd6bad8bfcc31a79914b7a28a9783f3aa
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2014-10-30 14:18:28 +0000

    Create Stock_Maximize.java

---
+++ Algorithms/Dynamic-Programming/Stock_Maximize.java
@@ -0,0 +1,34 @@
+package Algorithms_Dynamic_Programming;
+
+import java.util.Scanner;
+
+public class Stock_Maximize {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int numOfTestCase = scn.nextInt();
+
+		for (int i = 0; i < numOfTestCase; i++) {
+			int n = scn.nextInt();
+			long profit = 0;
+			int[] stockPrice = new int[n];
+
+			for (int j = 0; j < n; j++) {
+				stockPrice[j] = scn.nextInt();
+			}
+
+			int currMax = Integer.MIN_VALUE;
+
+			for (int j = n - 1; j >= 0; j--) {
+				if (currMax < stockPrice[j]) {
+					currMax = stockPrice[j];
+				}
+
+				profit += (currMax - stockPrice[j]);
+			}
+
+			System.out.println(profit);
+		}
+
+		scn.close();
+	}
+}
