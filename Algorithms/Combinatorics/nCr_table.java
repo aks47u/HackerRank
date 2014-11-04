e561cef58a030686fd31dc84437da459e2e7f802
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2014-11-04 09:33:53 +0000

    Create nCr_table.java

---
+++ Algorithms/Combinatorics/nCr_table.java
@@ -0,0 +1,62 @@
+package Algorithms_Combinatorics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class nCr_table {
+	BufferedReader reader;
+	StringTokenizer tokenizer;
+	PrintWriter out;
+
+	public static void main(String[] args) {
+		new nCr_table().run();
+	}
+
+	public void solve() throws IOException {
+		int T = nextInt();
+
+		for (int count = 0; count < T; count++) {
+			int N = nextInt();
+			BigInteger nCk = BigInteger.ONE;
+			int i = N;
+
+			for (int j = 0; j <= N; j++) {
+				System.out.print(nCk.mod(new BigInteger("1000000000")) + " ");
+				nCk = nCk.multiply(new BigInteger((i - j) + "")).divide(
+						new BigInteger(((j + 1)) + ""));
+			}
+
+			System.out.println();
+		}
+	}
+
+	public void run() {
+		try {
+			reader = new BufferedReader(new InputStreamReader(System.in));
+			tokenizer = null;
+			out = new PrintWriter(System.out);
+			solve();
+			reader.close();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	int nextInt() throws IOException {
+		return Integer.parseInt(nextToken());
+	}
+
+	String nextToken() throws IOException {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			tokenizer = new StringTokenizer(reader.readLine());
+		}
+
+		return tokenizer.nextToken();
+	}
+}
