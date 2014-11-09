86288def253b525b1345afae8f27273f785a9440
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2014-11-09 09:16:24 +0000

    Create Bash_Tutorials_Comparing_Numbers.sh

---
+++ The-Linux-Shell/Bash-Tutorials/Bash_Tutorials_Comparing_Numbers.sh
@@ -0,0 +1,17 @@
+read a
+read b
+
+if [ "$a" -gt "$b" ]
+then
+    echo "X is greater than Y"
+fi
+
+if [ "$a" -eq "$b" ]
+then
+    echo "X is equal to Y"
+fi
+
+if [ "$a" -lt "$b" ]
+then
+    echo "X is lesser than Y"
+fi
