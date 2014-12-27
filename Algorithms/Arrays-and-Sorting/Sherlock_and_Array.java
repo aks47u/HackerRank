0afd6f8dc0460c686b32a6cabdf83866d840f5e7
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2014-12-27 12:47:16 +0000

    Create Sherlock_and_Array.java

---
+++ Algorithms/Arrays-and-Sorting/Sherlock_and_Array.java
@@ -0,0 +1,41 @@
+package Algorithms_Arrays_and_Sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Sherlock_and_Array {
+	public static void main(String[] args) throws IOException {
+		StringBuffer sb = new StringBuffer();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (byte T = Byte.parseByte(br.readLine()); T > 0; --T) {
+			int N = Integer.parseInt(br.readLine());
+			short[] A = new short[N];
+			int i = 0;
+
+			for (String s : br.readLine().split(" ")) {
+				A[i++] = Short.parseShort(s);
+			}
+
+			int sum;
+			int[] S = new int[N];
+			sum = 0;
+
+			for (i = 0; i < N; ++i) {
+				S[i] = sum += A[i];
+			}
+
+			sum = 0;
+
+			for (i = N - 1; i >= 0; --i) {
+				S[i] -= sum += A[i];
+			}
+
+			sb.append((Arrays.binarySearch(S, 0) < 0) ? "NO\n" : "YES\n");
+		}
+
+		System.out.print(sb);
+	}
+}
