308f526fcc11bbeae87949e4e5ebaf6a9c75cfdf
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2014-12-29 12:43:06 +0000

    Create Sherlock_and_Watson.java

---
+++ Algorithms/Arrays-and-Sorting/Sherlock_and_Watson.java
@@ -0,0 +1,31 @@
+package Algorithms_Arrays_and_Sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Sherlock_and_Watson {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int K = Integer.parseInt(input[1]);
+		short Q = Short.parseShort(input[2]);
+		int[] A = new int[N];
+		input = br.readLine().split(" ");
+
+		for (int i = 0; i < N; ++i) {
+			A[i] = Integer.parseInt(input[i]);
+		}
+
+		int offset = N - K % N;
+		StringBuffer sb = new StringBuffer();
+
+		for (short q = 0; q < Q; ++q) {
+			int X = Integer.parseInt(br.readLine());
+			sb.append(A[(X + offset) % N] + "\n");
+		}
+
+		System.out.print(sb);
+	}
+}
