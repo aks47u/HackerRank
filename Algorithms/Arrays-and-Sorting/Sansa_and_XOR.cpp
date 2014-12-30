872054fba8dcfbaf3658dc5b5bae8dd1c3d98380
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2014-12-30 10:46:22 +0000

    Create Sansa_and_XOR.cpp

---
+++ Algorithms/Arrays-and-Sorting/Sansa_and_XOR.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+    
+	while (t--) {
+		int n;
+		cin >> n;
+		int ans = 0;
+        
+		for (int i = 0; i < n; i++) {
+			int num;
+			cin >> num;
+			long long times = (long long)(i + 1) * (n - i);
+            
+			if (times % 2 == 1) {
+				ans ^= num;
+            }
+		}
+        
+		cout << ans << endl;
+	}
+    
+	return 0;
+}
