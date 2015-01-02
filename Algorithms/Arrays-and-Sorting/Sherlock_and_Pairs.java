8c200c1736b9360b13b264e57a73cc8339b3a016
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-01-02 10:30:50 +0000

    Create Sherlock_and_Pairs.java

---
+++ Algorithms/Arrays-and-Sorting/Sherlock_and_Pairs.java
@@ -0,0 +1,41 @@
+package Algorithms_Arrays_and_Sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Sherlock_and_Pairs {
+	public static void main(String[] args) throws IOException {
+		StringBuffer sb = new StringBuffer();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (byte T = Byte.parseByte(br.readLine()); T > 0; --T) {
+			int N = Integer.parseInt(br.readLine());
+			int[] A = new int[N];
+			N = 0;
+
+			for (String Ai : br.readLine().split(" ")) {
+				A[N++] = Integer.parseInt(Ai);
+			}
+
+			Arrays.sort(A);
+			long sum = 0L;
+
+			for (int i = 0; i < N;) {
+				int j = i;
+
+				while (++j < N && A[j - 1] == A[j]) {
+				}
+
+				int count = j - i;
+				sum += count * (count - 1L);
+				i = j;
+			}
+
+			sb.append(sum + "\n");
+		}
+
+		System.out.print(sb);
+	}
+}
