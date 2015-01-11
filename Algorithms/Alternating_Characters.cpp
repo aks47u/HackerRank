4ac189b78a167d0a8a8e1acef786c2aa457be291
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-01-11 11:21:01 +0000

    Update Alternating_Characters.cpp

--- Algorithms/Alternating_Characters.cpp
+++ Algorithms/Alternating_Characters.cpp
@@ -5,31 +5,31 @@ using namespace std;
 int main() {
 	int t;
 	cin >> t;
-    
+
 	while (t--) {
 		string str;
 		cin >> str;
-		int len = (int)str.size();
+		int len = (int) str.size();
 		int cnt = 0, ans = 0;
-        
+
 		for (int i = 0; i < len; i++) {
 			if (i == 0 || str[i] == str[i - 1]) {
 				cnt++;
-            } else {
+			} else {
 				if (cnt > 1) {
 					ans += cnt - 1;
 				}
-                
+
 				cnt = 1;
 			}
 		}
-        
+
 		if (cnt > 1) {
 			ans += cnt - 1;
 		}
-        
+
 		cout << ans << endl;
 	}
-    
+
 	return 0;
 }
