76025012060703f85c3f3a3a3aab69af1fba24ca
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-01-17 10:36:37 +0000

    Create Find_Point.cpp

---
+++ Algorithms/Find_Point.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while (t--) {
+		int px, py, qx, qy;
+		cin >> px >> py >> qx >> qy;
+		cout << 2 * qx - px << " " << 2 * qy - py << endl;
+	}
+	
+	return 0;
+}
