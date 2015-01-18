3e89975542b64ede3d60873951eb61c5005de363
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-01-18 10:19:23 +0000

    Create Polar_Angles.py

---
+++ Algorithms/Polar_Angles.py
@@ -0,0 +1,17 @@
+import math
+n = input()
+
+def ang(x, y):
+    if math.atan2(y, x) < 0:
+        return (x, y, 2.0 * math.pi + math.atan2(y, x))
+    else:
+        return (x, y, math.atan2(y, x))
+
+M = []
+while n:
+    n -= 1
+    a, b = map(int, raw_input().split())
+    M.append(ang(a, b))
+
+for i in sorted(M, key=lambda x:(x[2], x[0] ** 2 + x[1] ** 2)):
+    print i[0], i[1]
