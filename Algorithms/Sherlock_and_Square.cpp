b9048457b9f4fa6f11a8ac8e2f47ddff842be300
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-01-20 09:58:12 +0000

    Create Sherlock_and_Square.cpp

---
+++ Algorithms/Sherlock_and_Square.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+const int MOD = 1000000007;
+
+int ModExp(int a, int b, int n) {
+	long long c = 1, d = a;
+	
+	while (b) {
+		if (b & 1) c = (c*d) % n;
+		
+		d = (d*d) % n;
+		b >>= 1;
+	}
+	
+	return (int)c;
+}
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while (t--) {
+		int n;
+		cin >> n;
+		cout << (ModExp(2, n + 1, MOD) + 2) % MOD << endl;
+	}
+	
+	return 0;
+}
