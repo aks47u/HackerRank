ec3e8b78f2e7a65562d7f71f3df9813b24df1fff
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-01-26 11:24:10 +0000

    Create Cipher.cpp

---
+++ Algorithms/Cipher.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int a[1000000];
+
+int main() {
+	int n, k;
+	scanf("%d %d\n", &n, &k);
+	int head = 0, curr = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (i - head == k) {
+			curr ^= a[head++];
+		}
+
+		a[i] = (getchar() - '0') ^ curr;
+		printf("%d", a[i]);
+		curr ^= a[i];
+	}
+
+	printf("\n");
+
+	return 0;
+}
