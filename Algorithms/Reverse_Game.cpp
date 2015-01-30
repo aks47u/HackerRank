8e5a5128e489b30b8b89002eb874917d85db10ba
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-01-30 10:50:07 +0000

    Create Reverse_Game.cpp

---
+++ Algorithms/Reverse_Game.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int pos[100000];
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+
+		for (int i = 0, j = n - 1; i < n; i += 2, j--) {
+			pos[j] = i;
+		}
+
+		for (int i = 1, j = 0; i < n; i += 2, j++) {
+			pos[j] = i;
+		}
+
+		cout << pos[k] << endl;
+	}
+
+	return 0;
+}
