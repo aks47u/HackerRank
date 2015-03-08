bdef99959946a257a4d7197cfa71fac22473740f
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-03-08 09:27:42 +0000

    Create Closest_Number.java

---
+++ Algorithms/Closest_Number.java
@@ -0,0 +1,31 @@
+package Algorithms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Closest_Number {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int x, a, b;
+		long storage;
+		String[] values = new String[3];
+		int testc = Integer.parseInt(br.readLine());
+
+		while (testc > 0) {
+			values = br.readLine().split(" ");
+			a = Integer.parseInt(values[0]);
+			b = Integer.parseInt(values[1]);
+			x = Integer.parseInt(values[2]);
+			storage = (long) Math.pow(a, b);
+
+			if (storage % x > x / 2) {
+				System.out.println(storage + x - storage % x);
+			} else {
+				System.out.println(storage - storage % x);
+			}
+
+			testc--;
+		}
+	}
+}
