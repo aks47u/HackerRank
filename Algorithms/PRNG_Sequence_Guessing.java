68e2723234c8b7b4ecec303ee94455a4d93552cf
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-03-23 10:28:52 +0000

    Create PRNG_Sequence_Guessing.java

---
+++ Algorithms/PRNG_Sequence_Guessing.java
@@ -0,0 +1,43 @@
+package Algorithms;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class PRNG_Sequence_Guessing {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt(), i = 0;
+		int[] a = new int[10];
+
+		for (; n > 0; n--) {
+			int x = scn.nextInt(), y = scn.nextInt();
+
+			for (i = 0; i < 10; i++) {
+				a[i] = scn.nextInt();
+			}
+
+			for (; x <= y; x++) {
+				Random rnd = new Random(x);
+
+				for (i = 0; i < 10; i++) {
+					if (a[i] != rnd.nextInt(1000)) {
+						break;
+					}
+				}
+
+				if (i == 10) {
+					System.out.print(x);
+
+					for (i = 0; i < 10; i++) {
+						System.out.print(" " + rnd.nextInt(1000));
+					}
+
+					System.out.println();
+					break;
+				}
+			}
+		}
+
+		scn.close();
+	}
+}
