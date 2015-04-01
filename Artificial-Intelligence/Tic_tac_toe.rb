f15b3fdb44076b8701e0b6e9d614598aa353ef43
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-04-01 10:05:42 +0100

    Update Tic_tac_toe.rb

--- Artificial-Intelligence/Tic_tac_toe.rb
+++ Artificial-Intelligence/Tic_tac_toe.rb
@@ -1,161 +1,111 @@
-def get_move_num(board)
-  move = 1;
-  for i in 0..2
-    for j in 0..2
-      if(board[i][j] != '_')
-        move = move + 1;
+class GameState
+  attr_accessor :current_player, :board, :moves, :rank, :player_figure, :current_move
+  
+  def initialize(current_player, board, player_figure = 'X')
+    self.current_player = current_player
+    self.board = board
+    self.player_figure = player_figure
+    self.moves = []
+    self.current_move = -999
+  end
+
+  def final_state?
+    winner || draw?
+  end
+
+  def draw?
+    board.compact.size == 9 && winner.nil?
+  end
+
+  def rank
+    @rank ||= final_state_rank || intermediate_state_rank
+  end
+
+  # this is only ever called when it's the AI's (the X player) turn
+  def next_move
+
+    moves.max_by {|x| x.rank }
+  end
+
+  def final_state_rank
+    if final_state?
+      return 0 if draw?
+      winner == player_figure ? 1 : -1
+    end
+  end
+
+  def intermediate_state_rank
+    # recursion, baby
+    ranks = moves.collect{ |game_state| game_state.rank }
+    if current_player == player_figure
+      ranks.max
+    else
+      ranks.min
+    end
+  end  
+
+  def winner
+    @winner ||= [
+     # horizontal wins
+     [0, 1, 2],
+     [3, 4, 5],
+     [6, 7, 8],
+
+     # vertical wins
+     [0, 3, 6],
+     [1, 4, 7],
+     [2, 5, 8],
+
+     # diagonal wins
+     [0, 4, 8],
+     [6, 4, 2]
+    ].collect { |positions|
+      ( board[positions[0]] == board[positions[1]] &&
+        board[positions[1]] == board[positions[2]] &&
+        board[positions[0]] ) || nil
+    }.compact.first
+  end
+end
+
+class GameTree
+  attr_accessor :player_figure
+  def generate(current_player, board, player_figure = 'X')
+    initial_game_state = GameState.new(current_player, board, player_figure)
+    self.player_figure = player_figure
+    generate_moves(initial_game_state)
+    initial_game_state
+  end
+
+  def generate_moves(game_state)
+    next_player = (game_state.current_player == 'X' ? 'O' : 'X')
+    game_state.board.each_with_index do |player_at_position, position|
+      unless player_at_position
+        next_board = game_state.board.dup
+        next_board[position] = game_state.current_player
+
+        next_game_state = GameState.new(next_player, next_board, player_figure)
+        next_game_state.current_move = position
+        game_state.moves << next_game_state
+        generate_moves(next_game_state)
       end
     end
   end
-  return move;
 end
-
-def can_complete(player,board)
-  if(board[0][0] == player && board[1][0] == player && board[2][0] == '_')
-    puts "2 0";
-  elsif(board[0][0] == player && board[2][0] == player && board[1][0] == '_')
-    puts "1 0";
-  elsif(board[0][0] == player && board[0][1] == player && board[0][2] == '_')
-    puts "0 2";
-  elsif(board[0][0] == player && board[0][2] == player && board[0][1] == '_')
-    puts "0 1";
-  elsif(board[0][0] == player && board[1][1] == player && board[2][2] == '_')
-    puts "2 2";
-  elsif(board[0][0] == player && board[2][2] == player && board[1][1] == '_')
-    puts "1 1";
-  elsif(board[0][2] == player && board[1][2] == player && board[2][2] == '_')
-    puts "2 2";
-  elsif(board[0][2] == player && board[2][2] == player && board[1][2] == '_')
-    puts "1 2";
-  elsif(board[0][2] == player && board[0][1] == player && board[0][0] == '_')
-    puts "0 0";
-  elsif(board[0][2] == player && board[0][0] == player && board[0][1] == '_')
-    puts "0 1";
-  elsif(board[0][2] == player && board[1][1] == player && board[2][0] == '_')
-    puts "2 0";
-  elsif(board[0][2] == player && board[2][0] == player && board[1][1] == '_')
-    puts "1 1";
-  elsif(board[2][0] == player && board[1][0] == player && board[0][0] == '_')
-    puts "0 0";
-  elsif(board[2][0] == player && board[0][0] == player && board[1][0] == '_')
-    puts "1 0";
-  elsif(board[2][0] == player && board[2][1] == player && board[2][2] == '_')
-    puts "2 2";
-  elsif(board[2][0] == player && board[2][2] == player && board[2][1] == '_')
-    puts "2 1";
-  elsif(board[2][0] == player && board[1][1] == player && board[0][2] == '_')
-    puts "0 2";
-  elsif(board[2][0] == player && board[0][2] == player && board[1][1] == '_')
-    puts "1 1";
-  elsif(board[2][2] == player && board[2][1] == player && board[2][0] == '_')
-    puts "2 0";
-  elsif(board[2][2] == player && board[2][0] == player && board[2][1] == '_')
-    puts "2 1";
-  elsif(board[2][2] == player && board[1][2] == player && board[0][2] == '_')
-    puts "0 2";
-  elsif(board[2][2] == player && board[0][2] == player && board[1][2] == '_')
-    puts "1 2";
-  elsif(board[2][2] == player && board[1][1] == player && board[0][0] == '_')
-    puts "0 0";
-  elsif(board[2][2] == player && board[0][0] == player && board[1][1] == '_')
-    puts "1 1";
-  elsif(board[1][1] == player && board[0][0] == player && board[2][2] == '_')
-    puts "2 2";
-  elsif(board[1][1] == player && board[0][1] == player && board[2][1] == '_')
-    puts "2 1";
-  elsif(board[1][1] == player && board[0][2] == player && board[2][0] == '_')
-    puts "2 0";
-  elsif(board[1][1] == player && board[1][0] == player && board[1][2] == '_')
-    puts "1 2";
-  elsif(board[1][1] == player && board[1][2] == player && board[1][0] == '_')
-    puts "1 0";
-  elsif(board[1][1] == player && board[2][0] == player && board[0][2] == '_')
-    puts "0 2";
-  elsif(board[1][1] == player && board[2][1] == player && board[0][1] == '_')
-    puts "0 1";
-  elsif(board[1][1] == player && board[2][2] == player && board[0][0] == '_')
-    puts "0 0";
-  else
-    return false;
-  end
-  return true;
-end
-
-def must_block(player,board)
-  if player == 'X'
-    return can_complete('O',board);
-  else
-    return can_complete('X',board);
-  end
-end
-
-def next_move(player,board,move)
-  completed = false;
-  blocking = false;
-  if(move == 1)
-    puts "2 0";
-  elsif(move == 2)
-    if(board[1][1] == 'X')
-      puts "2 2";
-    else
-      puts "1 1";
-    end
-  elsif(move == 3)
-    if(board[0][1] == 'O' || board[1][0] == 'O' || board[1][2] == 'O' || board[2][1] == 'O')
-      puts "1 1";
-    elsif(board[1][1] == 'O')
-      puts "0 2";
-    elsif(board[0][2] == 'O' || board[2][2] == 'O')
-      puts "0 0";
-    else
-      puts "2 2";
-    end
-  else
-    completed = can_complete(player,board)
-    if(completed == false)
-      blocking = must_block(player,board);
-      if(blocking == false)
-        prevent = can_complete(player,board)
-        if(move == 4 && (board[0][0] == 'X' || board[0][2] == 'X' || board[2][0] == 'X' || board[2][2] == 'X') && (board[0][1] == 'X' || board[1][0] == 'X' || board[1][2] == 'X' || board[2][1] == 'X'))
-          if(board[0][1] == 'X' || board[2][1] == 'X')
-            puts "1 0";
-          else
-            puts "0 1";
-          end
-        elsif(move == 4 && ((board[0][0] == 'X' && board[2][2] == 'X') || (board[0][2] == 'X' && board[2][0] == 'X') || (board[0][1] == 'X' && board[2][1] == 'X') || (board[1][0] == 'X' && board[1][2] == 'X')))
-          if((board[0][0] == 'X' && board[2][2] == 'X') || (board[0][2] == 'X' && board[2][0] == 'X'))
-            puts "0 1";
-          else
-            puts "0 0";
-          end
-        elsif(move == 6 && (board[2][1] == 'X' || board[0][1] == 'X') && (board[1][0] == 'X' || board[1][2] == 'X') && board[2][2] == '_')
-          puts "2 2";
-        elsif(board[0][0] == "_")
-          puts "0 0";
-        elsif(board[0][2] == "_")
-          puts "0 2";
-        elsif(board[2][2] == "_")
-          puts "2 2";
-        elsif(board[2][0] == "_")
-          puts "2 0";
-        elsif(board[0][1] == "_")
-          puts "0 1";
-        elsif(board[2][1] == "_")
-          puts "2 1";
-        elsif(board[1][0] == "_")
-          puts "1 0";
-        elsif(board[1][2] == "_")
-          puts "1 2";
-        else
-          puts "1 1";
-        end
-      end
-    end
-  end
+def opponent(player)
+    return 'O' if player == 'X'
+    
+    'X'
 end
 
 player = gets.chomp
+board = Array.new(3) { gets.scan(/\w/) }.flatten!
+board.map! { |e| e=='_' ? nil : e }
 
-board = Array.new(3) { gets.scan /\w/ }
-move = get_move_num(board);
-next_move(player,board,move);
+if board.compact.first.nil?
+  print '2 0' 
+elsif board.compact.length == 2 && board[6] == player && board[4] == opponent(player)
+  print '0 2'    
+else
+  step = GameTree.new.generate(player, board, player)
+  print step.next_move.current_move.to_s(3).rjust(2, '0').scan(/\w/).join(' ')
+end
