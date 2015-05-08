f1fe22beae9a15680a8ba9cba1174bde4162fbfb
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-05-08 11:33:32 +0100

    Create Find_Maximum_Index_Product.java

---
+++ Algorithms/Find_Maximum_Index_Product.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Find_Maximum_Index_Product {
+	public static void main(String[] args) throws IOException {
+		java.io.BufferedReader in = new java.io.BufferedReader(
+				new java.io.InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		long[] data = new long[n];
+
+		for (int i = 0; i < n; i++) {
+			data[i] = Long.parseLong(st.nextToken());
+		}
+
+		long max = 0;
+
+		for (int i = 0; i < data.length; i++) {
+			int left = 0;
+
+			for (int j = i - 1; j >= 0; j--) {
+				if (data[j] > data[i]) {
+					left = j + 1;
+					break;
+				}
+			}
+
+			if (left == 0) {
+				continue;
+			}
+
+			int right = 0;
+
+			for (int j = i + 1; j < data.length; j++) {
+				if (data[j] > data[i]) {
+					right = j + 1;
+					break;
+				}
+			}
+
+			long ans = (long) left * right;
+
+			if (max < ans) {
+				max = ans;
+			}
+		}
+
+		System.out.println(max);
+	}
+}
