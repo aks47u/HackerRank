6659a5ea8b7b3f399f050e9bb6846b017b56de26
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-05-23 11:47:01 +0100

    Create Basic_Data_Types.cpp

---
+++ Cpp/Basic_Data_Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a;
+	long b;
+	long long c;
+	char d;
+	float e;
+	double f;
+	cin >> a >> b >> c >> d >> e >> f;
+	cout << a << endl << b << endl << c << endl << d << endl;
+	printf("%f\n%lf\n", e, f);
+	
+	return 0;
+}
