b90b7231befcec962504d00365b629f51a5b0454
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-05-24 09:36:40 +0100

    Create Strings.cpp

---
+++ Cpp/Strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string a, b;
+	cin >> a >> b;
+	cout << a.length() << " " << b.length() << endl;
+	cout << a << b << endl;
+	swap(a[0], b[0]);
+	cout << a << " " << b << endl;
+	
+	return 0;
+}
