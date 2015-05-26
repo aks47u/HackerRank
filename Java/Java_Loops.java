9ad5efb5efb9f429eb9966758974bbf084e76c05
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-05-26 09:18:47 +0100

    Create Java_Loops.java

---
+++ Java/Java_Loops.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Java_Loops {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+
+		for (int i = 0; i < t; i++) {
+			int a = scn.nextInt();
+			int b = scn.nextInt();
+			int n = scn.nextInt();
+			long total = a;
+
+			for (int j = 0; j < n; j++) {
+				total += (Math.pow(2, j) * b);
+				System.out.print(total + " ");
+			}
+
+            System.out.println();
+		}
+
+		scn.close();
+	}
+}
