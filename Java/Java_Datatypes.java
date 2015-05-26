fb37725401b38542895d28840abe2155d0a66f5c
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-05-26 09:20:21 +0100

    One of the worst written questions I've ever seen on HackerRank

--- Java/Java_Datatypes.java
+++ Java/Java_Datatypes.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+public class Java_Datatypes {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int T = scn.nextInt();
