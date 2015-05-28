a184c5131e04fde158119fd3a6723f6ff6406bc3
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-05-28 08:28:31 +0100

    Create Java_BigInteger.java

---
+++ Java/Java_BigInteger.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		BigInteger a = scn.nextBigInteger();
+		BigInteger b = scn.nextBigInteger();
+		scn.close();
+		System.out.println(a.add(b));
+		System.out.println(a.multiply(b));
+	}
+}
