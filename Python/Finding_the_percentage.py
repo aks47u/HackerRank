730b2df2c2a6c2ab5d933bfdeb815c77692f2845
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-06-02 00:06:54 +0100

    Create Finding_the_percentage.py

---
+++ Python/Finding_the_percentage.py
@@ -0,0 +1,13 @@
+import fileinput
+
+lines = fileinput.input()
+grades = dict()
+N = int(lines[0])
+
+for i in range(1, N + 1):
+    line = lines[i].split(" ")
+    name = line.pop(0)
+    grades[name] = grades.get(i, []) + list(map(float, line))
+
+marks = grades[lines[N+1]]
+print("{0:.2f}".format(sum(marks)/len(marks)))
