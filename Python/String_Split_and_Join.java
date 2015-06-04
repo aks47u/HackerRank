14108a5184e0cf3dabae6987801b1bd089450ce1
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-06-04 22:44:46 +0100

    Create String_Split_and_Join.java

---
+++ Python/String_Split_and_Join.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println(scn.nextLine().replaceAll(" ", "-"));
+		scn.close();
+	}
+}
