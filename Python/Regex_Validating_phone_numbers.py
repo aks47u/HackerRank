5c6919956e51087d51d1a9be9e9ac61064d367c1
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-06-07 10:12:41 +0100

    Create Regex_Validating_phone_numbers.py

---
+++ Python/Regex_Validating_phone_numbers.py
@@ -0,0 +1,11 @@
+import fileinput
+import re
+
+def isMobileNum(numStr):
+  return "YES" if re.match(r"^[7-9]\d{9}$", numStr) else "NO"
+
+lines = fileinput.input()
+N = lines[0]
+
+for s in lines:
+  print(isMobileNum(s.strip()))
