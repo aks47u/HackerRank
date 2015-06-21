0b7d8aaf552f66a8f49277fc60232fb5849b8bd6
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-06-21 09:49:41 +0100

    Create functions.java

---
+++ Security/functions.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class functions {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println(scn.nextInt() % 11);
+		scn.close();
+	}
+}
