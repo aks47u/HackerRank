703a5aefd98a93c058a83cdd40d631cabdd80a00
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-06-21 09:50:13 +0100

    Create function_ii.java

---
+++ Security/function_ii.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class function_ii {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println((long) Math.pow(scn.nextInt(), 2));
+		scn.close();
+	}
+}
