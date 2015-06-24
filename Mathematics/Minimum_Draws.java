92ae79f1b3addcfabe6888f300b5f898a1bda573
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-06-24 08:36:06 +0100

    Create Minimum_Draws.java

---
+++ Mathematics/Minimum_Draws.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+	private static BufferedReader in = new BufferedReader(
+			new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws NumberFormatException,
+	Exception {
+		int T = Integer.parseInt(in.readLine());
+
+		for (int i = 0; i < T; i++) {
+			int noOfPairs = Integer.parseInt(in.readLine());
+			System.out.println(minimumDraw(noOfPairs));
+		}
+
+		in.close();
+	}
+
+	private static int minimumDraw(int noOfPairs) {
+		return (noOfPairs + 1);
+	}
+}
