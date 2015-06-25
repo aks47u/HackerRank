bb633bde7397544e8a3fb0d2fadeb81e8038155f
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-06-25 11:35:26 +0100

    Create No_Idea.py

---
+++ Python/No_Idea.py
@@ -0,0 +1,15 @@
+input()
+i = [int(item) for item in input().split()]
+a, b = (
+    {int(item) for item in input().split()} for _ in range(2)
+)
+
+happiness = 0
+
+for number in i:
+    if number in a:
+        happiness += 1
+    elif number in b:
+        happiness -= 1
+
+print(happiness)
