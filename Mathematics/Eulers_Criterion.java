edeb2c12eec4ddda8f6ac25fc1d85f0211b379b4
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-07-01 08:48:47 +0100

    Create Eulers_Criterion.java

---
+++ Mathematics/Eulers_Criterion.java
@@ -0,0 +1,193 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Eulers_Criterion {
+	private static Reader in;
+	private static PrintWriter out;
+
+	public static long mod_exp(long b, long e, long m) {
+		long r = 1;
+
+		while (e > 0) {
+			if ((e & 1) == 1) {
+				r = (r * b) % m;
+			}
+
+			b = (b * b) % m;
+			e >>= 1;
+		}
+
+		return r;
+	}
+
+	public static void main(String[] args) throws IOException {
+		in = new Reader();
+		out = new PrintWriter(System.out, true);
+		int T = in.nextInt();
+
+		while (T-- > 0) {
+			long A = in.nextLong(), M = in.nextLong();
+			out.println(A == 0 || M == 2 || mod_exp(A, (M - 1) / 2, M) == 1 ? "YES"
+					: "NO");
+		}
+	}
+
+	static class Reader {
+		final private int BUFFER_SIZE = 1 << 16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer, bytesRead;
+
+		public Reader() {
+			din = new DataInputStream(System.in);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public Reader(String file_name) throws IOException {
+			din = new DataInputStream(new FileInputStream(file_name));
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String readLine() throws IOException {
+			byte[] buf = new byte[1024];
+			int cnt = 0;
+			byte c = read();
+
+			while (c <= ' ') {
+				c = read();
+			}
+
+			do {
+				buf[cnt++] = c;
+			} while ((c = read()) != '\n');
+
+			return new String(buf, 0, cnt);
+		}
+
+		public String next() throws IOException {
+			byte[] buf = new byte[1024];
+			int cnt = 0;
+			byte c = read();
+
+			while (c <= ' ') {
+				c = read();
+
+			}
+
+			do {
+				buf[cnt++] = c;
+			} while ((c = read()) > ' ');
+
+			return new String(buf, 0, cnt);
+		}
+
+		public int nextInt() throws IOException {
+			int ret = 0;
+			byte c = read();
+
+			while (c <= ' ') {
+				c = read();
+			}
+
+			boolean neg = (c == '-');
+
+			if (neg) {
+				c = read();
+			}
+
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+
+			if (neg) {
+				return -ret;
+			}
+
+			return ret;
+		}
+
+		public long nextLong() throws IOException {
+			long ret = 0;
+			byte c = read();
+
+			while (c <= ' ') {
+				c = read();
+			}
+
+			boolean neg = (c == '-');
+
+			if (neg) {
+				c = read();
+			}
+
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+
+			if (neg) {
+				return -ret;
+			}
+
+			return ret;
+		}
+
+		public double nextDouble() throws IOException {
+			double ret = 0, div = 1;
+			byte c = read();
+
+			while (c <= ' ') {
+				c = read();
+			}
+
+			boolean neg = (c == '-');
+
+			if (neg) {
+				c = read();
+			}
+
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+
+			if (c == '.') {
+				while ((c = read()) >= '0' && c <= '9') {
+					ret += (c - '0') / (div *= 10);
+				}
+			}
+
+			if (neg) {
+				return -ret;
+			}
+
+			return ret;
+		}
+
+		private void fillBuffer() throws IOException {
+			bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+
+			if (bytesRead == -1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() throws IOException {
+			if (bufferPointer == bytesRead) {
+				fillBuffer();
+			}
+
+			return buffer[bufferPointer++];
+		}
+
+		public void close() throws IOException {
+			if (din == null) {
+				return;
+			}
+
+			din.close();
+		}
+	}
+}
