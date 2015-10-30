27d3097c4252fea8ee93edb9c926817e55ded4ed
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-10-30 10:35:58 +0000

    formatting

--- Algorithms/ACM_ICPC_Team.cpp
+++ Algorithms/ACM_ICPC_Team.cpp
@@ -1,32 +1,29 @@
-#include<cstdio>
-#include<vector>
-#include<bitset>
+#include <bitset>
+#include <cstdio>
+#include <vector>
+
 using namespace std;
 
 char str[1010];
 vector<bitset<1000>> v;
 
-int main()
-{
-	int n, m;
+int main() {
+	int n = 0, m = 0;
 	scanf("%d%d", &n, &m);
 	getchar();
-	
-	for (int i = 0; i < n; i++)
-	{
+
+	for (int i = 0; i < n; i++) {
 		scanf("%s", str);
-		bitset<1000> b(str);
+		bitset < 1000 > b(str);
 		v.push_back(b);
 	}
 
 	int best = 0, cnt = 0;
-	
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			bitset<1000> t = v[i] | v[j];
-			
+
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			bitset < 1000 > t = v[i] | v[j];
+
 			if (t.count() > best) {
 				best = t.count(), cnt = 1;
 			} else if (t.count() == best) {
@@ -34,9 +31,8 @@ int main()
 			}
 		}
 	}
-	
-	printf("%d\n", best);
-	printf("%d\n", cnt);
-	
+
+	cout << dest << endl << cnt << endl;
+
 	return 0;
 }
