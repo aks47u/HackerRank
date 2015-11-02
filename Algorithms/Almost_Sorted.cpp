1e4a0b27394bd4a8dec2d6c7ed63e5f59a3af2ee
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-11-02 08:09:19 +0000

    formatting

--- Algorithms/Almost_Sorted.cpp
+++ Algorithms/Almost_Sorted.cpp
@@ -1,10 +1,12 @@
-#include <iostream>
-#include <cstring>
-#include <vector>
 #include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 typedef long long LL;
+
 int n;
 vector<LL> a, b;
 
@@ -27,69 +29,69 @@ void print() {
 int main() {
 	cin >> n;
 	LL d;
-	
+
 	for (int i = 0; i < n; ++i) {
 		cin >> d;
 		a.push_back(d);
 		b.push_back(d);
 	}
-	
+
 	sort(b.begin(), b.end());
 	bool canSwap = true;
 	int p1 = -1, p2 = -1;
-	
+
 	for (int i = 0; i < n; ++i) {
 		if (a[i] == b[i]) {
 			continue;
 		}
-		
+
 		if (p1 == -1) {
 			p1 = i;
 			continue;
 		}
-		
+
 		if (p2 == -1) {
 			p2 = i;
 			continue;
 		}
-		
+
 		canSwap = false;
 		break;
 	}
 
 	if (p1 == -1) {
 		cout << "yes" << endl;
-		
+
 		return 0;
 	}
 
 	if (canSwap && p1 != -1 && p2 != -1 && b[p1] == a[p2] && b[p2] == a[p1]) {
 		cout << "yes" << endl;
 		cout << "swap " << p1 + 1 << " " << p2 + 1 << endl;
-		
+
 		return 0;
 	}
 
 	bool reversed = false, canReverse = true;
 	int i = 0;
-	
+
 	while (i < n - 1) {
 		if (a[i] < a[i + 1]) {
 			++i;
 			continue;
 		}
-		
+
 		if (reversed) {
 			canReverse = false;
 			break;
 		}
-		
+
 		int j = i;
-		
+
 		while (j < n - 1 && a[j] > a[j + 1]) {
 			++j;
 		}
-		
+
 		p1 = i;
 		p2 = j;
 		reverse(i, j);
