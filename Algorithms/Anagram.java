4b4818ff20567e48ce99e6da9be55fda4a42c476
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-11-04 09:57:37 +0000

    formatting

--- Algorithms/Anagram.java
+++ Algorithms/Anagram.java
@@ -18,12 +18,6 @@ public class Anagram {
 	public static void main(String[] args) throws IOException {
 		InputStream input = System.in;
 		PrintStream output = System.out;
-		File file = new File("a.in");
-
-		if (file.exists() && file.canRead()) {
-			input = new FileInputStream(file);
-		}
-
 		br = new BufferedReader(new InputStreamReader(input));
 		out = new PrintWriter(output);
 		solve();
