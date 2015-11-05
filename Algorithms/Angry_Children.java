8e0425a70ac2e5c987030d52963c25f53c4cbae0
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-11-05 09:41:35 +0000

    formatting

--- Algorithms/Angry_Children.java
+++ Algorithms/Angry_Children.java
@@ -3,11 +3,11 @@ package Algorithms;
 import java.util.Scanner;
 
 public class Angry_Children {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
 		int k = scn.nextInt();
-		int a[] = new int[n];
+		int[] a = new int[n];
 		int max = 0, min = a[0];
 
 		for (int i = 0; i < n; i++) {
@@ -19,7 +19,7 @@ public class Angry_Children {
 				min = a[i];
 			}
 		}
-		
+
 		scn.close();
 
 		if (k == 1) {
