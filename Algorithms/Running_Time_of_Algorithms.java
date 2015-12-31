5e705ba132eefe170bdabf0e16e21d344f83e217
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-12-31 12:11:02 +0000

    formatting

--- Algorithms/Running_Time_of_Algorithms.java
+++ Algorithms/Running_Time_of_Algorithms.java
@@ -7,28 +7,28 @@ public class Running_Time_of_Algorithms {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
 		int[] ar = new int[n];
-		
+
 		for (int i = 0; i < n; i++) {
 			ar[i] = scn.nextInt();
 		}
-		
-		System.out.println(insertionSort(ar));
+
 		scn.close();
+		System.out.println(insertionSort(ar));
 	}
 	
 	private static int insertionSort(int[] a) {
 		int noOfShifts = 0;
-		
+
 		for (int i = 1; i < a.length; i++) {
 			int value = a[i];
 			int j = i - 1;
-			
+
 			while (j >= 0 && a[j] > value) {
 				a[j + 1] = a[j];
 				j = j - 1;
 				noOfShifts++;
 			}
-			
+
 			a[j + 1] = value;
 		}
 
