9b77cc1f79f6d4947c351b82ffc40bfb828ebedf
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2016-01-12 13:24:36 +0000

    formatting and rework

--- Algorithms/Solve_me_second.java
+++ Algorithms/Solve_me_second.java
@@ -4,15 +4,13 @@ import java.util.Scanner;
 
 public class Solve_me_second {
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int T = scn.nextInt();
 
-		for (int i = 0; i < T; i++) {
-			int A = scn.nextInt();
-			int B = scn.nextInt();
-			System.out.println((A + B));
+		while (T-- > 0) {
+			System.out.println((sc.nextInt() + sc.nextInt()));
 		}
 
-		scn.close();
+		sc.close();
 	}
 }
