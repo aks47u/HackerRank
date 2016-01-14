a013f8ee4f8b502d224725d8451c5942cda7ff1e
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2016-01-14 09:26:44 +0000

    formatting and rework

--- Algorithms/Stock_Maximize.java
+++ Algorithms/Stock_Maximize.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class Stock_Maximize {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		int numOfTestCase = scn.nextInt();
+		int T = scn.nextInt();
 
-		for (int i = 0; i < numOfTestCase; i++) {
+		while (T-- > 0) {
 			int n = scn.nextInt();
 			long profit = 0;
 			int[] stockPrice = new int[n];
 
-			for (int j = 0; j < n; j++) {
-				stockPrice[j] = scn.nextInt();
+			for (int i = 0; i < n; i++) {
+				stockPrice[i] = scn.nextInt();
 			}
 
 			int currMax = Integer.MIN_VALUE;
 
-			for (int j = n - 1; j >= 0; j--) {
-				if (currMax < stockPrice[j]) {
-					currMax = stockPrice[j];
+			for (int i = n - 1; i >= 0; i--) {
+				if (currMax < stockPrice[i]) {
+					currMax = stockPrice[i];
 				}
 
-				profit += (currMax - stockPrice[j]);
+				profit += (currMax - stockPrice[i]);
 			}
 
 			System.out.println(profit);
