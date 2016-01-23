ad3724167db5a6abd335ae3e40462c9eb88bfd32
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2016-01-23 10:55:19 +0000

    formatting

--- Algorithms/Triangle_Numbers.cpp
+++ Algorithms/Triangle_Numbers.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    int t;
-    cin >> t;
-    
-    while (t--) {
+	int t;
+	cin >> t;
+
+	while (t--) {
 		long n;
 		cin >> n;
-		
-		if(n % 2 != 0) {
+
+		if (n % 2 != 0) {
 			cout << 2 << endl;
 		} else {
-			if((n / 2) % 2 == 0) {
+			if ((n / 2) % 2 == 0) {
 				cout << 3 << endl;
 			} else {
 				cout << 4 << endl;
-			}	    
+			}
 		}
-    }
-    
-    return 0;
+	}
+
+	return 0;
 }
