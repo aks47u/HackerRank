9bcdf1bacc49aae42388ab50413cd636dab7ea3a
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2016-03-07 07:57:07 +0000

    formatting and rework

--- Mathematics/K_Candy_Store.java
+++ Mathematics/K_Candy_Store.java
@@ -61,14 +61,6 @@ public class K_Candy_Store {
 		return Integer.parseInt(nextToken());
 	}
 
-	long nextLong() throws IOException {
-		return Long.parseLong(nextToken());
-	}
-
-	double nextDouble() throws IOException {
-		return Double.parseDouble(nextToken());
-	}
-
 	String nextToken() throws IOException {
 		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
 			tokenizer = new StringTokenizer(reader.readLine());
