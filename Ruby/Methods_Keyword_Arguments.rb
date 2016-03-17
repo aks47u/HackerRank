4730c4dadadc78e1999f0eb39bc4fff18b2f1a98
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2016-03-17 09:30:49 +0000

    formatting

--- Ruby/Methods_Keyword_Arguments.rb
+++ Ruby/Methods_Keyword_Arguments.rb
@@ -1,8 +1,8 @@
 def convert_to_celsius t, input_scale
     case input_scale.to_sym
-    when :celsius then t
-    when :kelvin then t-273.15
-    when :fahrenheit then (t-32)/1.8
+        when :celsius then t
+        when :kelvin then t-273.15
+        when :fahrenheit then (t-32)/1.8
     end
 end
 
@@ -12,8 +12,8 @@ def convert_temp t, input_scale: 'celsius', output_scale: 'celsius'
     t1 = convert_to_celsius(t, input_scale)
     
     case output_scale.to_sym
-    when :celsius then t1
-    when :kelvin then t1+273.15
-    when :fahrenheit then t1*1.8+32
+        when :celsius then t1
+        when :kelvin then t1+273.15
+        when :fahrenheit then t1*1.8+32
     end
 end
